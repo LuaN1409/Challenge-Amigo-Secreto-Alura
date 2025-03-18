7984e65c4185bcb6a5d79b2f7afee3ad144b02a7
LuaN1409 <marestrepoo@unal.edu.co>
2025-03-18 01:23:07 -0500

    V1

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+const inputAmigo = document.getElementById('amigo');
+const NombreAmigos = document.getElementById('listaAmigos');
+
+function agregarAmigo() {
+
+    if (inputAmigo.value === '') {
+        alert('Debes ingresar un nombre válido');
+        inputAmigo.value = '';
+        return;
+    }
+    else if (listaAmigos.includes(inputAmigo.value)) {
+        alert('El nombre ya está en la lista');
+        inputAmigo.value = '';
+        return;
+    }
+
+    listaAmigos.push(inputAmigo.value);
+    NombreAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+    inputAmigo.value = '';
+};
